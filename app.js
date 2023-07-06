daba0ef51b77e34545f99e4c74acd96b09d1f1b0
Hari-Kiri <pratamahari89@gmail.com>
2023-07-06 19:39:38 +0000

    Scaffold project.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+app.get('/read', (req, res) => {
+  res.send('Read service')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
