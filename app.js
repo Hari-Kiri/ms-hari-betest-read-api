cc075ec703f27344b4d0c4e7ecbe3256579e1b67
Hari-Kiri <pratamahari89@gmail.com>
2023-07-08 13:13:11 +0000

    Convert to ES Module & indent using tab.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
 const port = 80
 
 app.get('/read', (req, res) => {
-  res.send('Read service')
+  	res.send('Read service')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  	console.log(`Example app listening on port ${port}`)
 })
